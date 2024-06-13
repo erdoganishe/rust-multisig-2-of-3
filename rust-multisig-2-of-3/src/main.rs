5fffa8cb0f945f32a3dec9e96ab64266e731ed79
= <=>
2024-06-13 10:23:39 +0300

    test descriptor added?

--- rust-multisig-2-of-3/src/main.rs
+++ rust-multisig-2-of-3/src/main.rs
@@ -1,16 +1,19 @@
 use bdk::descriptor;
 use generate_test::generate;
+//use generate_test_3::generate_desc;
 use generate_wallet::generate_wallets;
 use script_generation::{generate_script, get_keys};
 
-mod generate_test_2;
+// mod generate_test_2;
+
+//mod generate_test_3;
+
 mod generate_wallet;
 mod script_generation;
 mod generate_test;
 fn main() {
     // let _ = generate_wallets();
     // let keys = get_keys();
-    let descriptor =  generate();
-    println!("{:#?}", descriptor);
+    generate_script();
    
 }
--- rust-multisig-2-of-3/src/script_generation.rs
+++ rust-multisig-2-of-3/src/script_generation.rs
@@ -1,4 +1,4 @@
-use bdk::bitcoin::Address;
+use bdk::bitcoin::{Address, PublicKey};
 use bdk::bitcoin::secp256k1::Secp256k1;
 use bdk::bitcoin::Script;
 use std::fs::File;
@@ -27,15 +27,17 @@ pub fn get_keys() -> Vec<String> {
     public_keys
 }
 
-pub fn generate_script()-> Descriptor<DescriptorPublicKey> {
-    let public_keys = get_keys();
+pub fn generate_script()-> Descriptor<PublicKey> {
+    //let public_keys = get_keys();
   
-    let pk1 = DescriptorPublicKey::from_str(&public_keys[0]).unwrap();
-    let pk2 = DescriptorPublicKey::from_str(&public_keys[1]).unwrap();
-    let pk3 = DescriptorPublicKey::from_str(&public_keys[2]).unwrap();
-
-    let descriptor_str = format!("wsh(multi(2,{},{},{}))", pk1, pk2, pk3);
-    let descriptor = Descriptor::<DescriptorPublicKey>::from_str(&descriptor_str).unwrap();
+    //let pk1 = DescriptorPublicKey::from_str(&public_keys[0]).unwrap();
+    //let pk2 = DescriptorPublicKey::from_str(&public_keys[1]).unwrap();
+    //let pk3 = DescriptorPublicKey::from_str(&public_keys[2]).unwrap();
+    let descriptor = Descriptor::<PublicKey>::from_str(
+        "wsh(multi(2,03789ed0bb717d88f7d321a368d905e7430207ebbd82bd342cf11ae157a7ace5fd,03dbc6764b8884a92e871274b87583e6d5c2a58819473e17e107ef3f6aa5a61626,03f28773c2d975288bc7d1d205c3748651b075fbc6610e58cddeeddf8f19405aa8))",
+    ).unwrap();
+    // let descriptor_str = format!("wsh(multi(2,{},{},{}))", pk1, pk2, pk3);
+    // let descriptor = Descriptor::<DescriptorPublicKey>::from_str(&descriptor_str).unwrap();
     
     println!("Descriptor: {}", descriptor);
     
